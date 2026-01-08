9da74d0d0a9a9bfdbfdb3fe01af75c1f961bf719
karthick <a.karthie+git@pm.me>
2026-01-08 12:33:56 +0000

    fix(io): threaded

--- coordinator/src/main.zig
+++ coordinator/src/main.zig
@@ -8,11 +8,10 @@ const std_thread = std.Io.Threaded;
 pub fn main() !void {
     var arena_allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena_allocator.deinit();
-    var thread = std_thread.init(arena_allocator.allocator());
-    var thread_io = thread.io();
-    defer thread.deinit();
-    // config file heartbeat interval
-    const h_beat = try config.read_config_from_file(thread_io, arena_allocator.allocator());
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    var threaded = std.Io.Threaded.init(arena.allocator(), .{});
+    defer threaded.deinit();
+    const h_beat = try config.read_config_from_file(threaded.io(), arena_allocator.allocator());
     print("heart beat:{d}\n", .{h_beat});
     const tcp_stream_server: tcp.tcp_server = .{};
     const tcp_opts = std.Io.net.IpAddress.ListenOptions{
@@ -20,15 +19,15 @@ pub fn main() !void {
         .mode = .stream,
         .protocol = .tcp,
     };
-    const tcp_stream = try tcp_stream_server.init(&thread_io, tcp_opts);
+    const tcp_stream = try tcp_stream_server.init(threaded.io(), tcp_opts);
     var udp_address = try std.Io.net.IpAddress.parse("224.0.0.1", 32100);
     const udp_opts: std.Io.net.IpAddress.BindOptions = .{ .ip6_only = false, .protocol = std.Io.net.Protocol.udp, .mode = std.Io.net.Socket.Mode.dgram };
-    const udp_socket = try udp_address.bind(thread_io, udp_opts);
-    defer udp_socket.close(thread_io);
+    const udp_socket = try udp_address.bind(threaded.io(), udp_opts);
+    defer udp_socket.close(threaded.io());
     while (true) {
-        _ = std.Io.async(thread_io, tcp.start_server, .{ tcp_stream, thread_io, tcp_opts });
+        _ = std.Io.async(threaded.io(), tcp.start_server, .{ tcp_stream, threaded.io(), tcp_opts });
         // const tcp_thread = try std.Thread.spawn(.{}, tcp.stream_data, .{tcp_stream});
-        _ = try std.Io.async(thread_io, send_heart_beat, .{ thread_io, udp_socket, &udp_address, "9999" });
+        _ = std.Io.async(threaded.io(), send_heart_beat, .{ threaded.io(), udp_socket, &udp_address, "9999" });
         // tcp_thread.join();
         // udp_thread.join();
     }
